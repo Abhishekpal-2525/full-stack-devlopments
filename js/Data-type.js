ca62de53aa3d4f4bc7bf725b5e043938fd129951
Abhishekpal-2525 <abhishekpal12234@gmail.com>
2025-08-18 23:55:41 +0530

    code

---
+++ js/Data-type.js
@@ -0,0 +1,18 @@
+var str ='string';
+var str ="string";
+var str =`string`;    //back-tick -> ternary operator  `${name}`
+
+var num =19;
+
+var bool = true;
+
+var arr =[1,2,3,4];            //array of number[]
+var arrOfobject =[{str: 'name'},{number:'2'}];        //array of object
+var obj = {name:'name',age:20};
+
+
+var  und =19;
+
+var nul = null;
+ 
+var sym = symbol();
