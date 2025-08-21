d7d5f01a949d951d247fcd8d1599b722b34b6bd3
Abhishekpal-2525 <abhishekpal12234@gmail.com>
2025-08-21 11:11:02 +0530

    code

---
+++ js/switch-case.js
@@ -0,0 +1,33 @@
+let day =1;
+switch (day){
+    case 1:
+        console.log("monday");
+        break;
+    
+    case 2:
+        console.log("tuesday");
+        break;
+
+    case 3:
+        console.log("wenday");
+        break;
+    
+    case 4:
+        consloe.log("thusday");
+         break;
+
+}
+
+
+let number=3;
+switch (number){
+    case 1:
+        consloe.log("addition");
+        break;
+    case 2:
+        console.log("subtraction");
+        break;
+    case 3:
+        console.log("multiple");
+        break;
+}
