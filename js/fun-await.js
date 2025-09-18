153d10d61d6114e247f2a48e6604f2ff63e47682
Abhishekpal-2525 <abhishekpal12234@gmail.com>
2025-09-18 11:05:52 +0530

    code

---
+++ js/fun-await.js
@@ -0,0 +1,21 @@
+function waiting_msg_promise(){
+
+    return new Promise((res,rej)=>{
+         setTimeout(()=>{
+            res("goatch you???")
+         },2000);
+    })
+}
+
+const async_await_show_msg_promise=async()=>{
+
+    console.log("start the task???");
+    let msg = await waiting_msg_promise();
+     console.log('msg',msg);
+
+     console.log("finsh the task!!!");
+     
+    
+}
+
+async_await_show_msg_promise();
