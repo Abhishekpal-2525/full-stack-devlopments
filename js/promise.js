c8c41f098b0b4d9077e8f3bcab4c73c9efc68211
Abhishekpal-2525 <abhishekpal12234@gmail.com>
2025-09-18 09:31:25 +0530

    code

---
+++ js/promise.js
@@ -0,0 +1,20 @@
+// let ice_cream = new Promise((res,rej)=>{
+//     let got_ice_cream = false;
+//     if(got_ice_cream) res(got_ice_cream);
+//     else rej(got_ice_cream);
+// })
+// console.log('ice_cream', ice_cream);
+
+// ice_cream.then(()=>{console.log('got the icream')}).catch(()=>{console.log('not get')})
+
+
+console.log('A');
+ 
+let ice_cream = new Promise((res,rej)=>{
+    let got_ice_cream;
+
+    console.log('B');
+
+setTimeout(()=>{})
+    
+})
