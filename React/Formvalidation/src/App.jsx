ad4968e4bd1f7f4abd8c91c6bed1621c0f5828a4
Abhishekpal-2525 <abhishekpal12234@gmail.com>
2025-12-16 00:07:50 +0530

    react code

---
+++ React/Formvalidation/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  const field =[
+    {
+    name: 'userName',
+    label:'user name',
+    type:'text',
+    isRequire:'true',
+    placeholder:'enter your '
+  }
+]; 
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ React/PropsOverview/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <PropsOverview components=(1)
+       name="abhishekpal"
+       number={1234567}
+       
+    </>
+  )
+}
+
+export default App
