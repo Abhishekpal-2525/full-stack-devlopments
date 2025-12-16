893c9ac7cb3513e6389c60ac09a20b3fd1b525e9
Abhishekpal-2525 <abhishekpal12234@gmail.com>
2025-12-16 16:24:53 +0530

    react code

--- React/Todotest/src/App.jsx
+++ React/Todotest/src/App.jsx
@@ -1,26 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-
-  const [todo , settodo]=useState(" ")
-
-   const handleAdd= ()=>{
-
-  
-   }
-  
-
-
-
-
-
   return (
     <>
-     <h1>Todo list</h1>
-     <input type text/>
-     <button onClick={handleAdd}>add</button>
-     <button>delete</button>
+    
     </>
   )
 }
