2e936b1ebb4f9a3527be9afdcb2dccc0626b509e
Abhishekpal-2525 <abhishekpal12234@gmail.com>
2026-01-16 18:02:25 +0530

    react code

--- React/PasswordGenerator/src/App.jsx
+++ React/PasswordGenerator/src/App.jsx
@@ -1,11 +1,19 @@
-
+import {useState}from'react'
 import './App.css'
 
 function App() {
-  
+
+  const[length , setLength]= useState(8)
+  const[numberAllowed, setNumberAllowed]= useState(false);
+  const[charAllowed, setCharAllowed]= useState(false);
+  const[password, setPassword]= useState('')
+
+  const passwordGenerator = useCallback(()=>{
+  });
 
   return (
     <>
+     <h1 className='text-center text-3xl text-white' > Password generator</h1>
      
     </>
   )
